d2a4cd409a7561c2e2b920388fc7479efd39508f
Gurdeep Kaur <kaur12@outlook.com>
2020-07-24 14:27:59 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <title>Articles lists</title>
+        </head>
+<body>
+
+<div id="app">
+<div class="container">
+<Articles/>
+
+</div>
+
+</div>
+
+<script src="./js/app.js"></script>
+
+    </body>
+
+
+</html>
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//List Article
+Route::get('articles', 'ArticleController@index');
+//List one Article
+Route::get('article/{id}', 'ArticleController@show') ;
+
+//create article
+Route::post('article', 'ArticleController@store');
+
+// update
+Route::put('article', 'ArticleController@store');
+//delete article
+Route::delete('article/{id}', 'ArticleController@destroy');
+
